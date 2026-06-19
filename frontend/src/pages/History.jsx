08bb27c5ebccf33eeea9ed188737f51d4632d85d
LAPTOP-KKJ37PKS\Utkarsh Tripathi <utkarshtripathi616@gmail.com>
2026-06-19 16:20:02 +0530

    Added Google OAuth login

---
+++ frontend/src/pages/History.jsx
@@ -0,0 +1,10 @@
+function History() {
+  return (
+    <div style={{ color: "white", padding: "20px" }}>
+      <h1>Search History</h1>
+      <p>Coming Soon...</p>
+    </div>
+  );
+}
+
+export default History;
